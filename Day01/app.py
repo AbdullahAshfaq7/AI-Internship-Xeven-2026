c8bf381c2ca8d09d68ce0fca8267f4488efc02e7
Abdullah Ashfaq <abdullahashfaq.it@gmail.com>
2026-05-13 01:19:06 +0500

    Day1: Inital Setup and Github Enviroment

---
+++ Day01/app.py
@@ -0,0 +1,6 @@
+# day01/app.py
+# My first Python script — AI Engineer Internship
+# Author: Abdullah | Date: 2026-05-13
+
+print("Hi, This is Abdullah, Future AI Engineer!")
+print("Starting my AI Engineering journey at Xeven Solutions.")
---
+++ README.md
@@ -0,0 +1,6 @@
+# AI Internship Xeven 2026
+
+This repository contains my 30-day AI Engineering internship journey, 
+-Python practice.
+-AI learning tasks.
+-Getting Familar with Dev tools.
